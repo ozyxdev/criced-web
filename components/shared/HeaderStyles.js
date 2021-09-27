11ffaaff80a3a39ada0955cd73d8e6bd1142015d
Omar Syx <jprzpam@gmail.com>
2021-09-27 12:52:21 -0500

    add preview and solve ssr styled components

---
+++ components/shared/HeaderStyles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+const HeaderStyles = styled.section`
+  margin-top: 4rem;
+  text-align: center;
+
+  @media (min-width: 768px) {
+    p {
+      width: 80%;
+      margin: 1rem auto;
+    }
+  }
+`
+
+export { HeaderStyles }
