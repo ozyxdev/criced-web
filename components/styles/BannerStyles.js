43887f9f9ca9263893f2da50f22013fc2185481c
Omar Syx <jprzpam@gmail.com>
2021-09-29 21:08:11 -0500

    add bannerImageSlice variation

--- components/styles/BannerStyles.js
+++ components/styles/BannerStyles.js
@@ -15,23 +15,16 @@ const BannerContainerStyles = styled.section`
     align-items: flex-start;
   }
 
+  .banner-description {
+    margin-bottom: 2rem;
+  }
+
   a {
     display: flex;
-    flex-basis: 0;
     padding: 1rem 2rem;
-    border-radius: 0.5rem;
-    color: var(--white);
-    background: var(--primary);
-    align-items: center;
-    margin-top: auto;
-    justify-items: space-between;
 
     span {
       margin-right: 1rem;
-    }
-
-    svg {
-      fill: var(--white);
     }
   }
 
@@ -51,6 +44,24 @@ const BannerContainerStyles = styled.section`
       grid-template-columns: repeat(5, 1fr);
       grid-template-rows: 1fr;
       grid-gap: 20px;
+    }
+  }
+
+  // Banner with image
+  &&.bannerImageSlice {
+    .container {
+      grid-template-columns: 1fr;
+    }
+    @media (min-width: 480px) {
+      .container {
+        grid-template-columns: 1fr 1fr;
+      }
+
+      .image-container {
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        grid-template-rows: repeat(3, 1fr);
+      }
     }
   }
 `
@@ -132,4 +143,50 @@ const BannerImageStyles = styled.div`
   }
 `
 
-export { BannerContainerStyles, BannerImageStyles }
+const PrimaryImageStyles = styled.div`
+  display: none;
+  @media (min-width: 480px) {
+    display: block;
+    background: red;
+    grid-column: 2/4;
+    grid-row: 1/4;
+    background-image: ${(props) => `url(${props.background})`};
+    background-size: cover;
+    background-position: center center;
+    height: 353px;
+    width: 270px;
+    border-radius: 1rem;
+    justify-self: end;
+
+    box-shadow: 0px 0px 3.6px rgba(0, 0, 0, 0.024),
+      0px 0px 10px rgba(0, 0, 0, 0.035), 0px 0px 24.1px rgba(0, 0, 0, 0.046),
+      0px 0px 80px rgba(0, 0, 0, 0.07);
+  }
+`
+const SecondaryImageStyles = styled.div`
+  display: none;
+  @media (min-width: 768px) {
+    display: block;
+    border-radius: 1rem;
+    width: 256px;
+    height: 151px;
+    background: blue;
+    grid-column: 1/3;
+    grid-row: 2/4;
+    background-image: ${(props) => `url(${props.background})`};
+    background-size: cover;
+    align-self: center;
+    justify-self: end;
+
+    box-shadow: 0px 0px 3.6px rgba(0, 0, 0, 0.024),
+      0px 0px 10px rgba(0, 0, 0, 0.035), 0px 0px 24.1px rgba(0, 0, 0, 0.046),
+      0px 0px 80px rgba(0, 0, 0, 0.07);
+  }
+`
+
+export {
+  BannerContainerStyles,
+  BannerImageStyles,
+  PrimaryImageStyles,
+  SecondaryImageStyles,
+}
--- slices/BannerSlice/index.js
+++ slices/BannerSlice/index.js
@@ -4,12 +4,14 @@ import PlaneIcon from '../../components/shared/icons/PlaneIcon'
 import {
   BannerContainerStyles,
   BannerImageStyles,
+  PrimaryImageStyles,
+  SecondaryImageStyles,
 } from '../../components/styles/BannerStyles'
 import { ButtonPrimary } from '../../components/styles/Button'
 
 export default function BannerSlice({ slice }) {
   return (
-    <BannerContainerStyles>
+    <BannerContainerStyles className={`${slice.variation}`}>
       <div className="container">
         <div className="content-container">
           <h1>{RichText.asText(slice.primary.title)}</h1>
@@ -23,17 +25,26 @@ export default function BannerSlice({ slice }) {
             </a>
           </ButtonPrimary>
         </div>
-        <div className="image-container">
-          {slice?.items?.map((item, i) => (
-            <BannerImageStyles
-              key={item.image.alt}
-              defaultBackground={item.image.url}
-              mobileBackground={item.image.mobile.url}
-            >
-              <RichText render={item.description} />
-            </BannerImageStyles>
-          ))}
-        </div>
+        {slice.variation !== 'bannerImageSlice' ? (
+          <div className="image-container">
+            {slice?.items?.map((item, i) => (
+              <BannerImageStyles
+                key={item.image.alt}
+                defaultBackground={item.image.url}
+                mobileBackground={item.image.mobile.url}
+              >
+                <RichText render={item.description} />
+              </BannerImageStyles>
+            ))}
+          </div>
+        ) : (
+          <div className="image-container">
+            <PrimaryImageStyles background={slice.primary.primaryImage.url} />
+            <SecondaryImageStyles
+              background={slice.primary.secondaryImage.url}
+            />
+          </div>
+        )}
       </div>
     </BannerContainerStyles>
   )
