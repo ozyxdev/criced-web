33aee89bbdcefeee20989573099b8190a422bca4
Omar Syx <jprzpam@gmail.com>
2021-10-19 13:22:08 -0500

    add preview to project

--- pages/cursos/[uid].js
+++ pages/cursos/[uid].js
@@ -1,18 +1,40 @@
 import SliceZone from 'next-slicezone'
 import { useGetStaticProps, useGetStaticPaths } from 'next-slicezone/hooks'
+import { useRouter } from 'next/router'
 import { Client } from '../../prismic-configuration'
+
+// Custom components
+import Loader from '../../components/Loader'
+import Custom404 from '../404'
+import useUpdatePreviewRef from '../../utils/useUpdatePreviewRef'
 
 import resolver from '../../sm-resolver'
 import { getAllCoursesWithSlug } from '../../utils/api'
 
-const Page = ({ slices }) => <SliceZone resolver={resolver} slices={slices} />
+const Page = ({ curso, previewRef }) => {
+  const router = useRouter()
+  if (router.isFallback) {
+    return <Loader />
+  }
 
-export const getStaticProps = async ({ params }) => {
-  const curso = await Client().getByUID('curso', params.uid)
+  if (!curso.id) {
+    return <Custom404 />
+  }
+
+  useUpdatePreviewRef(previewRef, curso.id)
+  return <SliceZone resolver={resolver} slices={curso.data.slices} />
+}
+
+export const getStaticProps = async ({ params, previewData }) => {
+  const previewRef = previewData ? previewData.ref : null
+  const refOption = previewRef ? { ref: previewRef } : null
+
+  const curso = await Client().getByUID('curso', params.uid, refOption)
 
   return {
     props: {
-      slices: curso.data.slices,
+      curso,
+      previewRef,
     },
   }
 }
